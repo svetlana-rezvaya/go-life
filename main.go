726211903beb3623577e36c992660f6535755a64
svetlana-rezvaya <svetlana-2305@mail.ru>
2020-11-12 22:02:58 +0300

    Add setCell function

--- main.go
+++ main.go
@@ -3,3 +3,7 @@ package main
 func getCell(field [][]bool, column int, row int) bool {
 	return field[row][column]
 }
+
+func setCell(field [][]bool, column int, row int, cell bool) {
+	field[row][column] = cell
+}
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_getCell_withTrue(test *testing.T) {
 	field := [][]bool{
@@ -22,6 +25,42 @@ func Test_getCell_withFalse(test *testing.T) {
 	}
 	cell := getCell(field, 0, 1)
 	if cell != false {
+		test.Fail()
+	}
+}
+
+func Test_setCell_withTrue(test *testing.T) {
+	field := [][]bool{
+		[]bool{false, false, false},
+		[]bool{false /* ! */, false, false},
+		[]bool{false, false, false},
+	}
+	setCell(field, 0, 1, true)
+
+	wantedField := [][]bool{
+		[]bool{false, false, false},
+		[]bool{true /* ! */, false, false},
+		[]bool{false, false, false},
+	}
+	if !reflect.DeepEqual(field, wantedField) {
+		test.Fail()
+	}
+}
+
+func Test_setCell_withFalse(test *testing.T) {
+	field := [][]bool{
+		[]bool{false, false, false},
+		[]bool{true /* ! */, false, false},
+		[]bool{false, false, false},
+	}
+	setCell(field, 0, 1, false)
+
+	wantedField := [][]bool{
+		[]bool{false, false, false},
+		[]bool{false /* ! */, false, false},
+		[]bool{false, false, false},
+	}
+	if !reflect.DeepEqual(field, wantedField) {
 		test.Fail()
 	}
 }
